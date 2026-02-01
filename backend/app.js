6fe4017ec206ea85a671b5e3e1094a9a6db77e7e
Saraswat-Deepika <deepika.saraswat770@gmail.com>
2026-02-01 15:07:24 +0530

    backend code and all file

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+module.exports = app;
